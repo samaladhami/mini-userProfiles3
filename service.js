38a9c4a883f6ba30073c09266a8dc1f949490205
samer <samaladhami@gmail.com>
2016-09-08 13:41:25 -0500

    mini

--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http , $q) {
 
   this.getUsers = function() {
-    return $http({
+    var dfd = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    }).then(function (response) {
+      var edit = response.data.data
+      for (var i = 0 ; i < edit.length ; i++) {
+        edit[i].first_name = 'Ralf'
+      }
+      dfd.resolve(edit)
+      dfd.resolve(response)
+    });
+    return dfd.promise;
+    }
 
 });
